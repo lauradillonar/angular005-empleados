e0311d9b968a0d95557f5e5b256c5f4b303f6048
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-02 18:17:15 -0300

    Listado de empleados - modificaciones - Curso PildorasInformaticas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular005-empleados'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular005-empleados');
+    expect(app.titulo).toEqual('angular005-empleados');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,13 @@ export class AppComponent {
     new Empleado("María", "Fernandez", "Jefa de sección", 3500),
     new Empleado("Laura", "López", "Administrativa", 2500)
   ];
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
 }
