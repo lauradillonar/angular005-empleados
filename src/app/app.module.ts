d289571c9461539121dbe076465f676a93e4a54b
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-03 19:10:04 -0300

    Directiva Input desde componente padre a hijo - Curso PildorasInformaticas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadoHijoComponent } from './empleado-hijo/empleado-hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoHijoComponent
   ],
   imports: [
     BrowserModule,
