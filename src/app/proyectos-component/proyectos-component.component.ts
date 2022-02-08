257c5e75bb3ec3a1914d4047fae6311680521c60
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-08 08:15:09 -0300

    Redireccionamiento automatico - Curso PildorasInformaticas

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -7,13 +10,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./proyectos-component.component.css']
 })
 export class ProyectosComponentComponent implements OnInit {
+  empleados: Empleado[]=[];
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService: EmpleadosService) { }
 
   ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
   }
 
   volverHome() {
+    this.router.navigate(['']);
+  }
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    //this.miServicio.muestraMensaje("Nombre del empleado: "+ miEmpleado.nombre);
+    //push miEmpleado
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
     this.router.navigate(['']);
   }
 
