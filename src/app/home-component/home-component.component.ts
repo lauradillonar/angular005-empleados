162bf16a391f47f4ddd105aecd95a6d039383075
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-07 21:28:28 -0300

    Routing programatico - Cursos PildorasInformaticas

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-home-component',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent implements OnInit {
+  titulo = 'Listado de empleados';
+  //array empleados
+  empleados: Empleado[]=[];
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0
 
-  constructor() { }
+
+  constructor(private miServicio:ServicioEmpleadosService, private empleadosService: EmpleadosService) { 
+    //this.empleados=this.empleadosService.empleados;
+  }
 
   ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
+  }
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    //this.miServicio.muestraMensaje("Nombre del empleado: "+ miEmpleado.nombre);
+    //push miEmpleado
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
   }
 
 }
