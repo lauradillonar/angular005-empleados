97a3e28a89b01b52fae4ee1860d85cc0b4a03620
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-08 16:09:37 -0300

    Actualizando registro segun valor que se pasa como parametro - Curso PildorasInformaticas

--- src/app/actualiza-component/actualiza-component.component.ts
+++ src/app/actualiza-component/actualiza-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Empleado } from '../empleado.model';
 import { EmpleadosService } from '../empleados.service';
 import { ServicioEmpleadosService } from '../servicio-empleados.service';
@@ -16,22 +16,29 @@ export class ActualizaComponentComponent implements OnInit {
   cuadroApellido:string="";
   cuadroCargo:string="";
   cuadroSalario:number=0
+  indice:number;
 
-  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService: EmpleadosService) { }
+  constructor(private router:Router,private route:ActivatedRoute, private miServicio:ServicioEmpleadosService, private empleadosService: EmpleadosService) { }
 
   ngOnInit(): void {
     this.empleados=this.empleadosService.empleados;
+    this.indice=this.route.snapshot.params['id'];
+    let empleado:Empleado=this.empleadosService.encontrarEmpleado(this.indice);
+    this.cuadroNombre=empleado.nombre;
+    this.cuadroApellido=empleado.apellido;
+    this.cuadroCargo=empleado.cargo;
+    this.cuadroSalario=empleado.salario;
   }
 
   volverHome() {
     this.router.navigate(['']);
   }
 
-  agregarEmpleado(){
+  actualizaEmpleado(){
     let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
     //this.miServicio.muestraMensaje("Nombre del empleado: "+ miEmpleado.nombre);
     //push miEmpleado
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+    this.empleadosService.actualizarEmpleado(this.indice, miEmpleado);
     this.router.navigate(['']);
   }
 
