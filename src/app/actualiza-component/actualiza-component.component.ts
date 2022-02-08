c2e2c13b693d3b2f9f964f046855c35fcc1bc1b8
lauradillonar <laura.dillon.ar@gmail.com>
2022-02-08 10:20:23 -0300

    Pasando parametros a rutas con Angular - Curso PildorasInformaticas

---
+++ src/app/actualiza-component/actualiza-component.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+
+@Component({
+  selector: 'app-actualiza-component',
+  templateUrl: './actualiza-component.component.html',
+  styleUrls: ['./actualiza-component.component.css']
+})
+export class ActualizaComponentComponent implements OnInit {
+
+  empleados: Empleado[]=[];
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0
+
+  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService: EmpleadosService) { }
+
+  ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
+  }
+
+  volverHome() {
+    this.router.navigate(['']);
+  }
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    //this.miServicio.muestraMensaje("Nombre del empleado: "+ miEmpleado.nombre);
+    //push miEmpleado
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+    this.router.navigate(['']);
+  }
+
+}
